3bec888b08e06da634a3e3bf7790efc1a4874423
cowperr <58703418+cowperr@users.noreply.github.com>
2024-12-10 15:08:57 +0000

    Update 01_02_movie_prototype.js

--- start/01_02_movie_prototype.js
+++ start/01_02_movie_prototype.js
@@ -1,1 +1,10 @@
 // Write your code here
+class Movie {
+  constructor(title, director, genre, releaseYear, rating) {
+    this.title = title;
+    this.director = director;
+    this.genre = genre;
+    this.releaseYear = releaseYear;
+    this.rating = rating;
+  }
+    
